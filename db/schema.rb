200409b1c96ed65681b93a2a8842eb2a7d811aee
Diego Stiehl <seccomiro@gmail.com>
2020-12-04 14:58:48 -0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,58 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_015828) do
+ActiveRecord::Schema.define(version: 2020_12_04_160331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.bigint "problem_id", null: false
+    t.string "description"
+    t.boolean "correct"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "selected", default: false
+    t.datetime "answered_at"
+    t.integer "order"
+    t.index ["problem_id"], name: "index_answers_on_problem_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "problems", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.string "difficulty"
+    t.string "question"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "game_id", null: false
+    t.integer "score", default: 0
+    t.index ["category_id"], name: "index_problems_on_category_id"
+    t.index ["game_id"], name: "index_problems_on_game_id"
+  end
 
   create_table "tokens", force: :cascade do |t|
     t.string "api_token"
     t.datetime "last_used_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "game_id", null: false
+    t.index ["game_id"], name: "index_tokens_on_game_id"
   end
 
+  add_foreign_key "answers", "problems"
+  add_foreign_key "problems", "categories"
+  add_foreign_key "problems", "games"
+  add_foreign_key "tokens", "games"
 end
