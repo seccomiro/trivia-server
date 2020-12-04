468d69882ba6f8a7261340a2621fa3f6ed41a87c
Diego Stiehl <seccomiro@gmail.com>
2020-12-04 15:31:54 -0300

    Add user to Game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ApplicationRecord
   has_many :problems, dependent: :destroy
   has_many :tokens, dependent: :destroy
+  belongs_to :user
 
   def score
     problems.reduce(0) { |total, problem| total + problem.score }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_160331) do
+ActiveRecord::Schema.define(version: 2020_12_04_183020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2020_12_04_160331) do
   create_table "games", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_games_on_user_id"
   end
 
   create_table "problems", force: :cascade do |t|
@@ -60,7 +62,20 @@ ActiveRecord::Schema.define(version: 2020_12_04_160331) do
     t.index ["game_id"], name: "index_tokens_on_game_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "answers", "problems"
+  add_foreign_key "games", "users"
   add_foreign_key "problems", "categories"
   add_foreign_key "problems", "games"
   add_foreign_key "tokens", "games"
