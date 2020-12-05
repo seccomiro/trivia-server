4c31365a3f67cc587aa7c09ae51404ef03030a96
Diego Stiehl <seccomiro@gmail.com>
2020-12-04 23:53:48 -0300

    Validates name at User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :games, dependent: :destroy
+  validates :name, presence: true
 
   def current_game
     games.playing.last
