f1d2f3318ee615d5dc1a0c16ee9c1877b1060ec3
Diego Stiehl <seccomiro@gmail.com>
2020-12-05 00:07:56 -0300

    Show game cretion state

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,16 +2,15 @@ class GamesController < ApiController
   def current
     category = Category.find_by(id: params[:category_id])
     difficulty = params[:difficulty]
-    @game = current_user.current_or_new_game(category, difficulty)
-  end
 
-  def score
-    @game = current_user.current_game
-
-    error = 'You are not playing a game' unless @game
-    return render_error(error) if error
-
-    render :current
+    if current_user.current_game
+      @game = current_user.current_game
+      @creation = :existing_game
+    else
+      @game = current_user.new_game(category, difficulty)
+      @creation = :new_game
+    end
+    # @game = current_user.current_or_new_game(category, difficulty)
   end
 
   def finish
--- app/views/games/current.json.jbuilder
+++ app/views/games/current.json.jbuilder
@@ -1,6 +1,7 @@
 json.status :success
 json.data do
   json.game do
+    json.creation @creation
     json.status @game.status
     json.started_at @game.created_at
     json.score @game.score
