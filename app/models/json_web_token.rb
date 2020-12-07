b9e6056aaaba10bbb9438a6390a005e6026d3f01
Diego Stiehl <seccomiro@gmail.com>
2020-12-06 23:51:30 -0300

    Change environment variable to use

--- app/models/json_web_token.rb
+++ app/models/json_web_token.rb
@@ -1,10 +1,10 @@
 class JsonWebToken
   def self.encode(payload)
     expiration = 2.weeks.from_now.to_i
-    JWT.encode payload.merge(exp: expiration), Rails.application.credentials.fetch(:secret_key_base)
+    JWT.encode payload.merge(exp: expiration), ENV['SECRET_KEY_BASE']
   end
 
   def self.decode(token)
-    JWT.decode(token, Rails.application.credentials.fetch(:secret_key_base)).first
+    JWT.decode(token, ENV['SECRET_KEY_BASE']).first
   end
 end
